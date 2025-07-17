7d231dba737649ae5f0a0848f7005c7d21c5650b
Bernardo <bernardo.fichera@gmail.com>
2025-07-18 01:11:03 +0200

    Some cleaning and reordering of types.

--- geometric_bayesian/utils/types.py
+++ geometric_bayesian/utils/types.py
@@ -1,38 +1,36 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import jax
 from collections.abc import Callable, Iterable, Mapping
-from typing import Any, List, Tuple, Optional
+from typing import Any, List, Tuple, Optional, Self
+from jaxtyping import Array, Float, Int, Num, PRNGKeyArray, PyTree, PyTreeDef
 
-from jaxtyping import Array, Float, Int, Num, PRNGKeyArray, PyTree
-
-# vector, matrix, tensor size
-Size = Int[Array, "..."]
-
-# scalar
+# ====================================================================================================
+# FLOAT
+# ====================================================================================================
 Scalar = Float[Array, ""]
-
-# vector -> 1d float array
 Vector = Float[Array, "m"]
-
-# matrix -> 2d float array
 Matrix = Float[Array, "m n"]
-
-# generic tensor
 Tensor = Float[Array, "..."]
 
-# scalar-valued function
 ScalarFn = Callable[..., Scalar]
-
-# vector-valued function
 VectorFn = Callable[..., Vector]
 
-# graph def
-PyTreeDef = jax.tree_util.PyTreeDef
-
-# parameters pytree
-Params = PyTree[Num[Array, "..."]]
-
-# vector -> 1d int array
+# ====================================================================================================
+# INT
+# ====================================================================================================
+ScalarInt = Int[Array, ""]
 VectorInt = Int[Array, "m"]
+MatrixInt = Int[Array, "m n"]
+TensorInt = Int[Array, "..."]
+
+# ====================================================================================================
+# HELPERS
+# ====================================================================================================
+Size = VectorInt
+
+# ====================================================================================================
+# JAX
+# ====================================================================================================
+Key = PRNGKeyArray
+Params = PyTree[Num[Array, "..."]]
