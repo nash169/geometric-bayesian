5d5aaebac65ee8238c6c78a50a8d0bcdb8091ccd
Bernardo <bernardo.fichera@gmail.com>
2025-07-18 00:43:20 +0200

    Added sample method; mean params optional (improved strcture for zero mean distribtuion); various minor fixes.

--- geometric_bayesian/densities/multivariate_normal.py
+++ geometric_bayesian/densities/multivariate_normal.py
@@ -7,15 +7,16 @@ import jax
 import jax.numpy as jnp
 from jaxtyping import ScalarLike
 
-from geometric_bayesian.utils.types import Scalar, Vector, Matrix
+from geometric_bayesian.utils.types import Scalar, Vector, Matrix, Key, Int
+from geometric_bayesian.densities.abstract_density import AbstractDensity
 from geometric_bayesian.operators.linear_operator import LinearOperator
 
 
-class MultivariateNormal:
+class MultivariateNormal(AbstractDensity):
     def __init__(
         self,
-        mean: Scalar | Vector,
         cov: LinearOperator,
+        mean: Optional[Scalar | Vector] = None,
     ) -> None:
         r"""
         Define normal distribution.
@@ -23,128 +24,104 @@ class MultivariateNormal:
                  Vector             -> diagonal covariace
                  LinearOperator   -> full covariance
         """
-        # assert isinstance(mean, Scalar) or isinstance(mean, Vector)
-        self._mean = mean
-        assert isinstance(cov, LinearOperator)
         self._cov = cov
+        if mean is not None:
+            assert isinstance(mean, Scalar) or isinstance(mean, Vector), "Mean can only be a Vector or Scalar."
+            self._mean = mean
+        assert isinstance(cov, LinearOperator)
 
     def __call__(
         self,
-        x: Scalar | Vector
+        x: Scalar | Vector,
+        **kwargs
     ) -> Scalar | Vector:
         r"""
         Evaluate density.
         """
-        return jnp.exp(self.log(x))
+        return -0.5 * (self._cov.logdet(**kwargs) + self._cov.size()[0] * jnp.log(jnp.pi) + self._cov.invquad(x - self._mean if hasattr(self, "_mean") else x, **kwargs))
 
-    def log(
+    def sample(
         self,
-        x: Vector | Matrix
-    ) -> Scalar | Vector:
-        if isinstance(x, Vector):
-            return self._log(x)
-        elif isinstance(x, Matrix):
-            return jax.vmap(self._log, in_axes=(0,))(x)
-        else:
-            msg = "Input type not valid [ Vector | Matrix ]"
-            ValueError(msg)
+        rng_key: Optional[Key] = None,
+        size: Optional[Int] = 1,
+        **kwargs
+    ):
+        if rng_key is None:
+            rng_key = jax.random.key(0)
+        rv = jax.random.normal(key=rng_key, shape=(self._cov.shape[0], ) if size == 1 else (self._cov.shape[0], size))
+        samples = self._cov.squareroot(**kwargs) @ rv
+        if hasattr(self, "_mean"):
+            samples += self._mean if size == 1 else self._mean[:, None]
+        return samples
 
-    # def logpdf_jvp(
-    #     self,
-    #     x: Vector | Matrix,
-    #     v: Vector | Matrix
-    # ) -> Scalar | Vector:
-    #     r"""
-    #     Returns jacobian-vector product the log probability density.
-    #     Note that when the mean is a scalar cov is
-    #     """
-    #     jvp = self._logpdf_jvp
-    #
-    #     if isinstance(x, Matrix):
-    #         jvp = jax.vmap(jvp, in_axes=(0, None), out_axes=0)
-    #
-    #     if isinstance(v, Matrix):
-    #         jvp = jax.vmap(jvp, in_axes=(None, 1), out_axes=1)
-    #
-    #     return jvp(x, v)
-
-    # def logpdf_jvp_mean(
-    #     self,
-    #     x: Vector | Matrix,
-    #     v: Vector | Matrix
-    # ) -> Scalar | Vector:
-    #     r"""
-    #     Returns jacobian-vector product the log probability density.
-    #     Note that when the mean is a scalar cov is
-    #     """
-    #     jvp_mean = self._logpdf_jvp_mean
-    #
-    #     if isinstance(x, Matrix):
-    #         jvp_mean = jax.vmap(jvp_mean, in_axes=(0, None), out_axes=0)
-    #
-    #     if isinstance(v, Matrix):
-    #         jvp_mean = jax.vmap(jvp_mean, in_axes=(None, 1), out_axes=1)
-    #
-    #     return jvp_mean(x, v)
-
-    def _log(
-        self,
-        x: Vector
-    ) -> Scalar:
-        return -0.5*(jnp.log(self._cov.det()) + self._cov.size()[0]*jnp.log(math.pi) + self._cov.inv_quad(x-self._mean))
-
-    def _jvp(
+    def jvp(
         self,
         x: Vector,
-        v: Vector
+        v: Vector,
+        **kwargs
     ) -> Scalar:
-        return jnp.dot(self._cov.mv(self._mean-x), v)
+        r"""
+        Gradient with respect to the input.
+        """
+        return jnp.dot(self._cov.solve(x - self._mean if hasattr(self, "_mean") else x, **kwargs), v)
+
+    def hvp(
+        self,
+        x: Vector,
+        v: Vector,
+        **kwargs
+    ) -> Vector:
+        r"""
+        Hessian with respect to the input.
+        """
+        return -self._cov.solve(v, **kwargs)
+
+    def jvp_params(
+        self,
+        **kwargs
+    ) -> Tuple:
+        r"""
+        Return handles for gradient function with respect to the params.
+        """
+        return lambda x, v: self._jvp_mean(x, v, **kwargs), lambda x, v: self._jvp_cov(x, v, **kwargs)
+
+    def hvp_params(
+        self,
+        **kwargs
+    ) -> Tuple:
+        r"""
+        Return handles for hessian function with respect to the params.
+        """
+        return lambda x, v: self._hvp_mean(x, v, **kwargs), lambda x, v: self._hvp_cov(x, v, **kwargs)
 
     def _jvp_mean(
         self,
         x: Vector,
-        v: Vector
+        v: Vector,
+        **kwargs
     ) -> Scalar:
-        return jnp.dot(self._cov.mv(x-self._mean), v)
+        return jnp.dot(self._cov.solve(x - self._mean if hasattr(self, "_mean") else x, **kwargs), v)
 
     def _jvp_cov(
         self,
         x: Vector,
-        v: Vector
+        v: Vector,
+        **kwargs
     ) -> None:
-        msg = "Jacobian vector product with respect to covariance matric not implemented yet"
-        NotImplementedError(msg)
-
-    def _jvp_params(
-        self,
-        x: Vector,
-        v: Vector
-    ) -> Tuple:
-        return lambda x, v: self._jvp_mean(x, v), lambda x, v: self._jvp_cov(x, v)
-
-    def _hvp(
-        self,
-        x: Vector,
-        v: Vector
-    ) -> Vector:
-        return self._cov.solve(v)
+        NotImplementedError("Jacobian vector product with respect to covariance matric not implemented yet")
 
     def _hvp_mean(
         self,
         x: Vector,
-        v: Vector
+        v: Vector,
+        **kwargs
     ) -> Vector:
-        return -self._cov.solve(v)
+        return -self._cov.solve(v, **kwargs)
 
     def _hvp_cov(
         self,
         x: Vector,
-        v: Vector
+        v: Vector,
+        **kwargs
     ) -> None:
-        msg = "Hessian vector product with respect to covariance matric not implemented yet"
-        NotImplementedError(msg)
-
-    def _hvp_params(
-        self,
-    ) -> Tuple:
-        return lambda x, v: self._hvp_mean(x, v), lambda x, v: self._hvp_cov(x, v)
+        raise NotImplementedError("Hessian vector product with respect to covariance matric not implemented yet")
