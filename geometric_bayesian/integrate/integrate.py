a51c972a74a2166248221c103a0ae6c5a7d14e33
Bernardo <bernardo.fichera@gmail.com>
2025-07-18 00:53:38 +0200

    Move to state-space representation for dynamical systems integration; added support for time and control input dependent dynamical systems.

--- geometric_bayesian/integrate/integrate.py
+++ geometric_bayesian/integrate/integrate.py
@@ -2,20 +2,42 @@
 # encoding: utf-8
 
 import jax
-from geometric_bayesian.utils.types import Callable, Optional
-from geometric_bayesian.integrate.euler_forward import euler_forward
+import jax.numpy as jnp
+from geometric_bayesian.utils.types import Callable, Optional, Array, Float
+from geometric_bayesian.integrate.ef import ef
 
 
 def integrate(
     f: Callable,
-    dt: Optional[float] = 0.01,
-    T: Optional[float] = 1.0,
-    integrator: Optional[Callable] = euler_forward,
+    u: Optional[Callable | Array] = None,
+    dt: Optional[Float] = 0.01,
+    T: Optional[Float] = 1.0,
+    integrator: Optional[Callable] = ef,
 ):
     assert integrator is not None and dt is not None and T is not None
 
-    def step(carry, _):
-        x, v = integrator(f=f, x=carry[0], v=carry[1], dt=0.01)
-        return (x, v), (x, v)
+    def step(carry, i):
+        t = i * dt
+        x = integrator(f=f, t=t, x=carry[0], u=u, dt=dt)
+        return (x, t), (x, )
 
-    return lambda x, v: jax.lax.scan(step, (x, v), None, length=int(T/dt))[1]
+    return lambda x: jax.lax.scan(step, (x, 0.), jnp.arange(int(T / dt)))[1]
+
+# def euler_forward(f, x, v, dt):
+#     vn = v + dt*f(x, v)
+#     xn = x + dt*v
+#     return xn, vn
+#
+# def integrate(
+#     f: Callable,
+#     dt: Optional[float] = 0.01,
+#     T: Optional[float] = 1.0,
+#     integrator: Optional[Callable] = euler_forward,
+# ):
+#     assert integrator is not None and dt is not None and T is not None
+#
+#     def step(carry, _):
+#         x, v = integrator(f=f, x=carry[0], v=carry[1], dt=0.01)
+#         return (x, v), (x, v)
+#
+#     return lambda x, v: jax.lax.scan(step, (x, v), None, length=int(T / dt))[1]
