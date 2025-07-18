8dd6279fe17bc5a24a8776655bfcabbc536c53e6
Bernardo <bernardo.fichera@gmail.com>
2025-07-18 09:13:29 +0200

    Summation operator; it covers most common sums in Bayesian inference. Scales back to naive (dense) operations as default.

--- geometric_bayesian/operators/sum_operator.py
+++ geometric_bayesian/operators/sum_operator.py
@@ -2,13 +2,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from typing import Self
-from ..types import Size, Scalar, Array, Vector, Matrix
-from .linear_operator import LinearOperator
-
 import jax
 import jax.numpy as jnp
-from jax.scipy.sparse.linalg import cg
+
+from geometric_bayesian.utils.types import Size, Scalar, Array, Vector, Matrix, Self
+from geometric_bayesian.operators.linear_operator import LinearOperator
+from geometric_bayesian.operators import *
 
 
 class SumOperator(LinearOperator):
@@ -19,7 +18,8 @@ class SumOperator(LinearOperator):
         r"""
         Define set of arrays needed for the linear operator
         """
-        self._ops = ops  # check that all the operators have the same dimension
+        assert all(ops[0].shape == ops[1].shape), "Error: cannot sum operators with different dimension."
+        self._ops = ops
 
     def size(
         self
@@ -57,15 +57,49 @@ class SumOperator(LinearOperator):
         r"""
         Return solve of the linear operator
         """
-        y = jnp.zeros(self.size()[0])
-        for op in self._ops:
-            y += op.solve(vec, **kwargs)
-        return y
+        if any(isinstance(op, LowRankOperator) for op in self._ops) and any(isinstance(op, DiagOperator) for op in self._ops):
+            from geometric_bayesian.linalg.woodbury_solve import woodbury_solve
+            lowrank_op = [op for op in self._ops if isinstance(op, LowRankOperator)][0]
+            diag_op = [op for op in self._ops if isinstance(op, DiagOperator)][0]
+            return woodbury_solve(lowrank_op.right, lowrank_op.diag, diag_op.diag, vec)
+        elif any(isinstance(op, PSDOperator) for op in self._ops) and any(isinstance(op, DiagOperator) for op in self._ops):
+            from geometric_bayesian.linalg.woodbury_solve import woodbury_chol_solve
+            psd_op = [op for op in self._ops if isinstance(op, PSDOperator)][0]
+            diag_op = [op for op in self._ops if isinstance(op, DiagOperator)][0]
+            return woodbury_chol_solve(psd_op._op, diag_op.diag, vec)
+        else:
+            raise NotImplementedError(f"Method not implemented.")
 
-    def to_dense(
+    def diagonalize(
         self,
-    ) -> Matrix:
-        r"""
-        Return dense matrix representation of the linear operator
-        """
-        return jax.vmap(self.mv, in_axes=0)(jnp.eye(self.size()[1]))
+        **kwargs
+    ) -> tuple[Vector, Matrix]:
+        if any(issubclass(type(op), SymOperator) for op in self._ops):
+            from geometric_bayesian.linalg.diagonalize import diagonalize
+            return diagonalize(self.mv, self.shape[0], **kwargs)
+        else:
+            return super().diagonalize()
+
+    def lowrank(
+        self,
+        **kwargs
+    ) -> LinearOperator:
+        from geometric_bayesian.operators.low_rank_operator import LowRankOperator
+        if all(issubclass(type(op), (LowRankOperator, SymOperator)) for op in self._ops):
+            eigval, eigvec = self.diagonalize(**kwargs)
+            return LowRankOperator(diag=eigval, right=eigvec)
+        else:
+            return super().lowrank()
+
+    def squareroot(
+        self,
+        **kwargs
+    ) -> LinearOperator:
+        if all(issubclass(type(op), (LowRankOperator, SymOperator)) for op in self._ops):
+            if self.shape[0] * self.shape[1] <= 1e4:
+                from geometric_bayesian.operators import DenseOperator
+                return DenseOperator(jnp.linalg.cholesky(self.dense()._mat))
+            else:
+                return self.lowrank(**kwargs).squareroot()
+        else:
+            return super().squareroot()
