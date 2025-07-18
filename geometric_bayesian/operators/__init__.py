439c9e3421e18f8400cd95dd6466171c79fe8d4b
Bernardo <bernardo.fichera@gmail.com>
2025-07-18 09:23:58 +0200

    Added all operators.

--- geometric_bayesian/operators/__init__.py
+++ geometric_bayesian/operators/__init__.py
@@ -5,5 +5,13 @@ from .dense_operator import DenseOperator
 from .sym_operator import SymOperator
 from .psd_operator import PSDOperator
 from .diag_operator import DiagOperator
+from .low_rank_operator import LowRankOperator
 
-__all__ = ["DenseOperator", "SymOperator", "PSDOperator", "DiagOperator"]
+__all__ = [
+    "DenseOperator",
+    "SymOperator",
+    "PSDOperator",
+    "DiagOperator",
+    "LowRankOperator",
+    "InvOperator"
+]
