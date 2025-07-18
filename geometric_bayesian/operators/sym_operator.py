Improved diagonalize method (to be moved outside the operator) and fixed related log-determinant calculation.

geometric_bayesian/operators/sym_operator.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import jax
 import jax.numpy as jnp
 from geometric_bayesian.operators.linear_operator import LinearOperator
-from geometric_bayesian.utils.types import Scalar, Vector
+from geometric_bayesian.utils.types import Matrix, Scalar, Tuple, Optional, Key, Vector
+
+from geometric_bayesian.linalg.eigvec_tridiagonal import eigvec_tridiagonal
 from geometric_bayesian.linalg.lanczos import lanczos
-from jax.scipy.linalg import eigh_tridiagonal
 from jax.experimental.sparse.linalg import lobpcg_standard
 
 
@@ -18,24 +20,44 @@ class SymOperator(LinearOperator):
 
     def diagonalize(
         self,
-        num_iterations: int,
-        rng_key
-    ) -> Vector:
+        rng_key: Optional[Key] = None,
+        num_modes: Optional[int] = None,
+        num_iterations: Optional[int] = None,
+        method: Optional[str] = None
+    ) -> Tuple[Vector, Matrix]:
         r"""
         Return eigenvalues of the linear operator
         Eigenvectors calculation not available (https://github.com/jax-ml/jax/issues/14019)
         """
-        # lobpcg_standard(op, jax.random.uniform(rng_key, (op.size()[0], 1)), m=100)
-        d, e, v = lanczos(self.mv, self.size()[0], num_iterations, rng_key)
+        if rng_key is None:
+            rng_key = jax.random.key(0)
+        if num_modes is None:
+            num_modes = (0.3 * self.shape[0]).astype(int)
+        # assert num_modes <= self.size()[0], f"Number of modes requested exceeds opeartor dimension [{self.size()[0]}]"
+        if num_iterations is None:
+            num_iterations = 10
+        if method is None:
+            method = 'lanczos'
 
-        return eigh_tridiagonal(d, e, eigvals_only=True)
+        if method == 'lanczos':
+            alpha, beta, v = lanczos(self.mv, self.size()[0], num_modes, rng_key)
+            d = jax.scipy.linalg.eigh_tridiagonal(alpha, beta, eigvals_only=True)
+            v = jnp.matmul(v.T, eigvec_tridiagonal(rng_key, alpha, beta, d))
+        elif method == 'lobpcg':
+            assert num_iterations is not None
+            mv_batch = jax.vmap(self.mv, in_axes=(1,), out_axes=1)
+            d, v, _ = lobpcg_standard(mv_batch, jax.random.uniform(rng_key, shape=(self.size()[0], num_modes)), m=num_iterations)
+        else:
+            msg = "provide valid method ['lanczos', 'lobpcg']"
+            ValueError(msg)
 
-    def log_det(
+        return d, v
+
+    def logdet(
         self,
-        num_iteration: int,
-        rng_key
+        **kwargs
     ) -> Scalar:
         r"""
         Return log determinat via stocastich Lanczos quadrature
         """
-        return jnp.sum(jnp.log(self.diagonalize(num_iteration, rng_key)))
+        return jnp.sum(jnp.log(self.diagonalize(**kwargs)[0]))