28d558cae0abd286d9c7043414253495bb7d79df
Bernardo <bernardo.fichera@gmail.com>
2025-07-18 08:57:48 +0200

    Added categorical distribution.

--- geometric_bayesian/densities/__init__.py
+++ geometric_bayesian/densities/__init__.py
@@ -4,5 +4,6 @@
 from geometric_bayesian.densities.normal import Normal
 from geometric_bayesian.densities.multivariate_normal import MultivariateNormal
 from geometric_bayesian.densities.bernoulli import Bernoulli
+from geometric_bayesian.densities.categorical import Categorical
 
-__all__ = ["Normal", "MultivariateNormal", "Bernoulli"]
+__all__ = ["Normal", "MultivariateNormal", "Bernoulli", "Categorical"]
