6a4d908cadb526416185ea23607a8c0e5d1c3d27
Bernardo <bernardo.fichera@gmail.com>
2025-07-18 08:53:42 +0200

    Various fixes and renamed methods.

--- geometric_bayesian/densities/bernoulli.py
+++ geometric_bayesian/densities/bernoulli.py
@@ -15,7 +15,7 @@ class Bernoulli:
         logits: Optional[bool] = True
     ) -> None:
         r"""
-        Define categorical distribution.
+        Define Bernoulli distribution.
         """
         self._mu, self._logits = mu, logits
 
@@ -23,52 +23,48 @@ class Bernoulli:
         self,
         x: Scalar
     ) -> Scalar:
-        return jnp.power(self._mu, x)*jnp.power(1-self._mu, 1-x)
+        log_sig_mu = jax.nn.log_sigmoid(self._mu) if self._logits else jnp.log(self._mu)
+        log_sig_not_mu = jax.nn.log_sigmoid(-self._mu) if self._logits else jnp.log(1 - self._mu)
+        return x * log_sig_mu + (1 - x) * log_sig_not_mu
 
-    def _log(
-        self,
-        x: Scalar
-    ) -> Scalar:
-        log_mu = jax.nn.log_sigmoid(self._mu) if self._logits else jnp.log(self._mu)
-        log_not_mu = jax.nn.log_sigmoid(-self._mu) if self._logits else jnp.log(1-self._mu)
-        return x*log_mu + (1-x)*log_not_mu
-
-    def _jvp(
+    def jvp(
         self,
         x: Scalar,
         v: Scalar
     ) -> Scalar:
-        log_mu = jax.nn.log_sigmoid(self._mu) if self._logits else jnp.log(self._mu)
-        log_not_mu = jax.nn.log_sigmoid(-self._mu) if self._logits else jnp.log(1-self._mu)
-        return (log_mu - log_not_mu)*v
+        log_sig_mu = jax.nn.log_sigmoid(self._mu) if self._logits else jnp.log(self._mu)
+        log_sig_not_mu = jax.nn.log_sigmoid(-self._mu) if self._logits else jnp.log(1 - self._mu)
+        return (log_sig_mu - log_sig_not_mu) * v
 
-    def _hvp(
+    def hvp(
         self,
         x: Scalar,
         v: Scalar
     ) -> Scalar:
-        return jnp.array(0.0)
+        return jnp.array([0.0])
+
+    def jvp_params(
+        self,
+    ) -> Tuple:
+        return (lambda x, v, : self._jvp_mu(x, v),)
+
+    def hvp_params(
+        self,
+    ) -> Tuple:
+        return (lambda x, v: self._hvp_mu(x, v),)
 
     def _jvp_mu(
         self,
         x: Scalar,
         v: Scalar
     ) -> Scalar:
-        return (x/self._mu - (1-x)/(1-self._mu))*v
-
-    def _jvp_params(
-        self,
-    ) -> Tuple:
-        return (lambda x, v, : self._jvp_mu(x, v),)
+        log_dev_mu = x - jax.nn.sigmoid(self._mu) if self._logits else x / self._mu - (1 - x) / (1 - self._mu)
+        return log_dev_mu * v
 
     def _hvp_mu(
         self,
         x: Scalar,
         v: Scalar
     ) -> Scalar:
-        return -(x*jnp.power(self._mu, -2) + (1-x)*jnp.power(1-self._mu, 2))*v
-
-    def _hvp_params(
-        self,
-    ) -> Tuple:
-        return (lambda x, v: self._hvp_mu(x, v),)
+        log_hess_mu = -jax.nn.sigmoid(self._mu) * (1 - jax.nn.sigmoid(self._mu)) if self._logits else - x / self._mu**2 - (1 - x) / (1 - self._mu)**2
+        return log_hess_mu * v
