bb0ffc655702f0836e5cd6e6553c698a56996aec
Bernardo <bernardo.fichera@gmail.com>
2025-07-18 09:23:37 +0200

    Added some structure to define mandatory, default and optional methods.

--- geometric_bayesian/operators/linear_operator.py
+++ geometric_bayesian/operators/linear_operator.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from abc import ABC, abstractmethod
-from typing import Self
-
-from geometric_bayesian.utils.types import Size, Scalar, Array, Vector, Matrix
-
+import warnings
 import jax
 import jax.numpy as jnp
+from abc import ABC, abstractmethod
+from geometric_bayesian.utils.types import Array, Self, Scalar, Vector, Matrix, VectorInt
 
 
 class LinearOperator(ABC):
@@ -24,7 +22,7 @@ class LinearOperator(ABC):
     @abstractmethod
     def size(
         self
-    ) -> Size:
+    ) -> VectorInt:
         r"""
         Return size of the linear operator
         """
@@ -33,7 +31,8 @@ class LinearOperator(ABC):
     @abstractmethod
     def mv(
         self,
-        vec: Vector
+        vec: Vector,
+        **kwargs
     ) -> Vector:
         r"""
         Return matrix-vector multiplication of the linear operator
@@ -47,23 +46,20 @@ class LinearOperator(ABC):
         r"""
         Return transposed matrix-vector multiplication of the linear operator
         """
-        pass
+        raise NotImplementedError("The class {} requires a transpose function!".format(self.__class__.__name__))
 
-    def __call__(
+# ====================================================================================================
+# Default methods preferably to override
+# ====================================================================================================
+    def dense(
         self,
-        input: Vector | Matrix
-    ) -> Vector:
+    ) -> Self:
         r"""
-        Overload () operator
+        Return dense matrix representation of the linear operator
         """
-        return self.mv(input) if isinstance(input, Vector) else jax.vmap(self.mv, in_axes=0)(input)
-
-    def __add__(
-        self,
-        other
-    ):
-        from geometric_bayesian.operators.sum_operator import SumOperator
-        return SumOperator(self, other)
+        warnings.warn("Default `dense` methods.")
+        from geometric_bayesian.operators.dense_operator import DenseOperator
+        return DenseOperator(self(jnp.eye(self.size()[1])))
 
     def solve(
         self,
@@ -73,9 +69,10 @@ class LinearOperator(ABC):
         r"""
         Return solve of the linear operator
         """
-        return self.to_dense().solve(vec, **kwargs)
+        warnings.warn("Default `solve` methods.")
+        return self.dense().solve(vec, **kwargs)
 
-    def inv_quad(
+    def invquad(
         self,
         vec: Vector,
         **kwargs
@@ -83,22 +80,97 @@ class LinearOperator(ABC):
         r"""
         Return x^T A^-1 x for the linear operator A
         """
+        warnings.warn("Default `invquad` methods.")
         return jnp.dot(vec, self.solve(vec, **kwargs))
 
-    def to_dense(
+    def logdet(
+        self,
+    ) -> Scalar:
+        r"""
+        Return log determinant of the linear operator
+        """
+        if self.shape[0] == self.shape[1]:
+            warnings.warn("Default `logdet` methods.")
+            return jnp.linalg.slogdet(self.dense())
+        else:
+            raise NotImplementedError(f"Method not implemented.")
+
+    def inverse(
         self,
     ) -> Self:
         r"""
-        Return dense matrix representation of the linear operator
+        Return inverse operator
         """
-        from geometric_bayesian.operators.dense_operator import DenseOperator
-        return DenseOperator(self(jnp.eye(self.size()[1])))
+        warnings.warn("Default `inverse` methods.")
+        mv, solve = self.mv, self.solve
+        self.mv, self.solve = solve, mv
+        return self
 
-    # @abstractmethod
-    # def det(
-    #     self,
-    # ) -> Scalar:
-    #     r"""
-    #     Return determinant of the linear operator
-    #     """
-    #     pass
+# ====================================================================================================
+# Operator specific methods
+# ====================================================================================================
+    def diagonalize(
+        self,
+    ) -> tuple[Vector, Matrix]:
+        r"""
+        Return determinant of the linear operator
+        """
+        raise NotImplementedError(f"Method not implemented.")
+
+    def lowrank(
+        self,
+    ) -> Self:
+        r"""
+        Return low rank operator
+        """
+        raise NotImplementedError(f"Method not implemented.")
+
+    def squareroot(
+        self,
+    ) -> Self:
+        r"""
+        Return low rank operator
+        """
+        raise NotImplementedError(f"Method not implemented.")
+
+# ====================================================================================================
+# Function Operator overloads
+# ====================================================================================================
+    def __call__(
+        self,
+        x: Vector | Matrix
+    ) -> Vector:
+        r"""
+        Overload () operator
+        """
+        return self.mv(x) if x.ndim == 1 else jax.vmap(self.mv, 1, 1)(x)
+
+    def __matmul__(self, other):
+        if isinstance(other, jax.Array):
+            return self(other)
+        else:
+            raise NotImplementedError(f"Matrix-Vector multiplication not supported for vector type {type(other)}")
+
+    def __add__(
+        self,
+        other
+    ):
+        if isinstance(other, LinearOperator):
+            from geometric_bayesian.operators.sum_operator import SumOperator
+            return SumOperator(self, other)
+        else:
+            raise NotImplementedError(f"Addition not supported for type {type(other)}")
+
+    def __mul__(
+            self,
+            other
+    ):
+        if isinstance(other, LinearOperator):
+            from geometric_bayesian.operators.mul_operator import MulOperator
+            return MulOperator(self, other)
+        else:
+            raise NotImplementedError(f"Multiplication not supported for type {type(other)}")
+
+    @property
+    def shape(self) -> VectorInt:
+        return self.size()
