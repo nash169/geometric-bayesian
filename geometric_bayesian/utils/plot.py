07b91fb7b05ae0f651a2c04cd620d43264928ccb
Bernardo <bernardo.fichera@gmail.com>
2025-11-14 17:35:06 +0100

    Added some plot helper.

--- geometric_bayesian/utils/plot.py
+++ geometric_bayesian/utils/plot.py
@@ -11,70 +11,103 @@ from itertools import combinations
 from geometric_bayesian.utils.types import Callable
 
 
-def colorbar(im, fig, ax, pos="left", size="8%", pad=0.4, label=None, labelpad=-50, ticks=None):
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes(pos, size=size, pad=pad)
-    cbar = fig.colorbar(im, cax=cax, ticks=ticks, orientation='vertical')
-    cax.yaxis.set_ticks_position(pos)
-    cbar.outline.set_visible(False)
-    cbar.set_ticks([0.0])
-    if label is not None:
-        cbar.set_label('$' + label + '$', fontsize=18, rotation=90, labelpad=labelpad)
-
-
-def get_xyz(fn, min, max, res):
-    if isinstance(fn, Callable):
-        x, y = jnp.meshgrid(jnp.linspace(min[0], max[0], res), jnp.linspace(min[1], max[1], res))
-        eval_fn = fn(jnp.vstack((x.ravel(), y.ravel())).transpose())
-        if len(eval_fn.shape) == 1:
-            z = eval_fn.reshape(res, res)
-        else:
-            x, y, z = eval_fn[:, 0].reshape(res, res), eval_fn[:, 1].reshape(res, res), eval_fn[:, 2].reshape(res, res)
+def simple_plot(
+    fn: Callable | jax.Array,
+    range=[-1, 1],
+    res=100,
+    fig=None,
+    **kwargs
+):
+    if fig is not None:
+        ax = fig.get_axes()
     else:
-        res = int(jnp.sqrt(fn.shape[0]))
-        x, y, z = fn[:, 0].reshape(res, res), fn[:, 1].reshape(res, res), fn[:, 2].reshape(res, res)
-    return x, y, z
+        fig, ax = plt.subplots()
+
+    if isinstance(fn, Callable):
+        x = jnp.linspace(range[0], range[1], res)
+        ax.plot(x, fn(x))
+    else:
+        ax.plot(*fn)
+
+    fig.tight_layout()
+    return fig
 
 
-def contour_plot(fn, grid=None, min=[-1, -1], max=[1, 1], res=100, iso=True, alpha=1.0, zorder=0, cbar=True, xlabel=None, ylabel=None, fig_ax=None, **kwargs):
-    fig, ax = plt.subplots() if fig_ax is None else fig_ax
+def scatter_plot(data, fig=None, **kwargs):
+    if fig is not None:
+        ax = fig.get_axes()
+    else:
+        if len(data) <= 2:
+            fig, ax = plt.subplots()
+        else:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(*data, **kwargs)
+    return fig
+
+
+def contour_plot(
+        fn,
+        grid=None,
+        min=[-1, -1],
+        max=[1, 1],
+        res=100,
+        xlabel=None,
+        ylabel=None,
+        iso=False,
+        cbar=False,
+        fig=None,
+        **kwargs
+):
+    if fig is not None:
+        ax = fig.get_axes()
+    else:
+        fig, ax = plt.subplots()
+
     if grid is None:
-        x, y, z = get_xyz(fn, min, max, res)
+        x, y, z = _get_xyz(fn, min, max, res)
     else:
         z = fn
         x, y = grid
-    im = ax.contourf(x, y, z, 500, cmap="Spectral", alpha=alpha, zorder=zorder)
-    if iso:
-        ax.contour(x, y, z, 10, cmap=None, colors='#f2e68f', zorder=zorder)
 
-    # remove frame
+    im = ax.contourf(x, y, z, 500, cmap="Spectral")
+    if iso:
+        ax.contour(x, y, z, 10, cmap=None, colors='#f2e68f')
+
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
     ax.spines['left'].set_visible(False)
-    # remove ticks
-    ax.get_xaxis().set_ticks([])
-    ax.get_yaxis().set_ticks([])
-    # ax.axes.get_xaxis().set_visible(False)
-    # ax.axes.get_yaxis().set_visible(False)
-    # ax.axis('off')
-    ax.axis('equal')
+    ax.set_xticks([x.min(), x.max()])
+    ax.set_yticks([y.min(), y.max()])
+    ax.yaxis.set_label_position("right")
+    ax.yaxis.tick_right()
     if xlabel is not None:
         ax.set_xlabel(xlabel, fontsize=20)
     if ylabel is not None:
         ax.set_ylabel(ylabel, fontsize=20)
+    ax.axis('equal')
 
     if cbar:
-        colorbar(im, fig, ax, **kwargs)
-    fig.patch.set_visible(False)
-    fig.tight_layout()
+        _colorbar(im, fig, ax, **kwargs)
 
+    # fig.patch.set_visible(False)
+    fig.tight_layout()
     return fig
 
 
-def contour_nd_plot(fn, ranges, resolution=50, reduce_fn=jnp.sum, iso=False, alpha=1.0, zorder=0, cbar=False, **kwargs):
+def contour_nd_plot(
+        fn,
+        ranges,
+        res=50,
+        reduce_fn=jnp.sum,
+        iso=False,
+        cbar=False,
+        axes_label=None,
+        **kwargs
+):
     dim = len(ranges)
-    grids = [jnp.linspace(start, end, resolution) for (start, end) in ranges]
+    grids = [jnp.linspace(start, end, res) for (start, end) in ranges]
     mesh = jnp.meshgrid(*grids, indexing='ij')
     # F = fn(*mesh)
     F = fn(jnp.stack([g.ravel() for g in mesh], axis=-1)).reshape(mesh[0].shape)
@@ -84,42 +117,39 @@ def contour_nd_plot(fn, ranges, resolution=50, reduce_fn=jnp.sum, iso=False, alp
     ncols = int(jnp.ceil(jnp.sqrt(n_plots)))
     nrows = int(jnp.ceil(n_plots / ncols))
 
-    fig, axes = plt.subplots(nrows, ncols)
-    axes = [item for row in axes for item in row]
-
+    fig, axs = plt.subplots(nrows, ncols)
+    axs = [item for row in axs for item in row]
     for idx, (i, j) in enumerate(pairs):
-        ax = axes[idx]
+        ax = axs[idx]
         reduce_axes = tuple(k for k in range(dim) if k not in (i, j))
         F_proj = reduce_fn(F, axis=reduce_axes)
-        xi = grids[i]
-        xj = grids[j]
-        Xi, Xj = jnp.meshgrid(xi, xj, indexing='ij')
 
-        im = ax.contourf(Xi, Xj, F_proj, levels=500, cmap="Spectral", alpha=alpha, zorder=zorder)
+        Xi, Xj = jnp.meshgrid(grids[i], grids[j], indexing='ij')
+        im = ax.contourf(Xi, Xj, F_proj, levels=500, cmap="Spectral")
         if iso:
-            ax.contour(Xi, Xj, F_proj, 10, cmap=None, colors='#f2e68f', zorder=zorder)
+            ax.contour(Xi, Xj, F_proj, 10, cmap=None, colors='#f2e68f')
 
-        # ax.axes.get_xaxis().set_visible(False)
-        # ax.axes.get_yaxis().set_visible(False)
-        ax.axis('equal')
-        # ax.axis('off')
-        ax.set_title(f"Projection onto axes ({i}, {j})")
-        ax.set_xlabel(f"x{i}", labelpad=-5)
-        ax.set_ylabel(f"x{j}", labelpad=-20)
+        # ax.set_title(f"Projection onto axes ({i}, {j})")
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['left'].set_visible(False)
         ax.set_xticks([Xi.min(), Xi.max()])
         ax.set_yticks([Xj.min(), Xj.max()])
-        ax.yaxis.set_label_position("right")
-        ax.yaxis.tick_right()
+        ax.set_xlabel(f"${axes_label}_1$", labelpad=-5)
+        ax.set_ylabel(f"${axes_label}_2$", labelpad=-20)
+        # ax.yaxis.set_label_position("right")
+        # ax.yaxis.tick_right()
+        ax.axis('equal')
 
         if cbar:
-            # fig.colorbar(im, ax=ax)
-            colorbar(im, fig, ax, **kwargs)
+            _colorbar(im, fig, ax, **kwargs)
 
-    for ax in axes[n_plots:]:
+    for ax in axs[n_plots:]:
         ax.axis('off')
-    fig.patch.set_visible(False)
-    fig.tight_layout()
 
+    # fig.patch.set_visible(False)
+    fig.tight_layout()
     return fig
 
 
@@ -139,6 +169,31 @@ def surf_plot(fn, min=[-1, -1], max=[1, 1], res=100, fig_ax=None, **kwargs):
     fig.tight_layout()
 
     return fig
+
+
+def _colorbar(im, fig, ax, pos="left", size="8%", pad=0.4, label=None, labelpad=-50, ticks=None):
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes(pos, size=size, pad=pad)
+    cbar = fig.colorbar(im, cax=cax, ticks=ticks, orientation='vertical')
+    cax.yaxis.set_ticks_position(pos)
+    cbar.outline.set_visible(False)
+    cbar.set_ticks([0.0])
+    if label is not None:
+        cbar.set_label('$' + label + '$', fontsize=18, rotation=90, labelpad=labelpad)
+
+
+def _get_xyz(fn, min, max, res):
+    if isinstance(fn, Callable):
+        x, y = jnp.meshgrid(jnp.linspace(min[0], max[0], res), jnp.linspace(min[1], max[1], res))
+        eval_fn = fn(jnp.vstack((x.ravel(), y.ravel())).transpose())
+        if len(eval_fn.shape) == 1:
+            z = eval_fn.reshape(res, res)
+        else:
+            x, y, z = eval_fn[:, 0].reshape(res, res), eval_fn[:, 1].reshape(res, res), eval_fn[:, 2].reshape(res, res)
+    else:
+        res = int(jnp.sqrt(fn.shape[0]))
+        x, y, z = fn[:, 0].reshape(res, res), fn[:, 1].reshape(res, res), fn[:, 2].reshape(res, res)
+    return x, y, z
 
 
 def plot_regression(model, X_test, y_test=None, X_train=None, y_train=None, fig=None):
