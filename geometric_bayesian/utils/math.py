Fixed outer Jacobian and added efficient Gram matrix-vec product.

geometric_bayesian/utils/math.py
@@ -1,8 +1,10 @@
+from chex import Array
 import jax
 import jax.numpy as jnp
 
-from geometric_bayesian.utils.types import Callable, Optional
+from geometric_bayesian.utils.types import Callable, Optional, PyTree, Tensor
 from geometric_bayesian.operators.linear_operator import LinearOperator
+from geometric_bayesian.utils.helper import pytree_to_array, array_to_pytree
 
 
 def hvp(
@@ -53,6 +55,28 @@ def outer_jacobian(
 ) -> Callable:
     def fn(x, v):
         _, jvp = jax.linearize(f, x)
-        hjtv = jax.linear_transpose(jvp, x)(v)[0] if h is None else h(jax.linear_transpose(jvp, x)(v)[0])
-        return jvp(hjtv)
+        jtv = jax.linear_transpose(jvp, x)(v)[0]
+        if h is not None:
+            leaves, tree_def = jax.tree.flatten(jtv)
+            shapes = [leaf.shape for leaf in leaves]
+            jtv = array_to_pytree(h(pytree_to_array(jtv)), (shapes, tree_def))
+        return jvp(jtv)
     return fn
+
+
+def gram(
+    k: Callable,
+    x: Tensor
+) -> Callable:
+    n = x.shape[0]
+    k_diag = jax.vmap(lambda i: k(x[i], x[i]))(jnp.arange(n))
+    i_off, j_off = jnp.triu_indices(n, k=1)
+    k_off = jax.vmap(lambda i, j: k(x[i], x[j]))(i_off, j_off)
+
+    def mv(v):
+        y = k_diag * v
+        y = y.at[i_off].add(k_off * v[j_off])
+        y = y.at[j_off].add(k_off * v[i_off])
+        return y
+
+    return mv