0975953a5f19a9d489b0b9ad22a650884396185f
Bernardo <bernardo.fichera@gmail.com>
2025-11-25 13:09:11 +0100

    Minors.

--- geometric_bayesian/kernels/__init__.py
+++ geometric_bayesian/kernels/__init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
+from geometric_bayesian.kernels.rbf import rbf
+from geometric_bayesian.kernels.ard import ard
+
+
+_all_ = []
--- geometric_bayesian/utils/__init__.py
+++ geometric_bayesian/utils/__init__.py
@@ -4,15 +4,16 @@
 from geometric_bayesian.utils.helper import (
     DataLoader,
 )
+
 from geometric_bayesian.utils.plot import (
+    simple_plot,
+    scatter_plot,
+    quiver_plot,
     contour_plot,
     surf_plot,
-    plot_regression,
 )
 
+from geometric_bayesian.utils.math import gram
+
 __all__ = [
-    "DataLoader",
-    "contour_plot",
-    "surf_plot",
-    "plot_regression",
 ]
