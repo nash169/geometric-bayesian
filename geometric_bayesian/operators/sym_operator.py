0ba20be9b11f0ad37bfe5cf5429df300ccece50f
Bernardo <bernardo.fichera@gmail.com>
2025-12-04 10:33:32 +0100

    Default to symeig.

--- geometric_bayesian/operators/sym_operator.py
+++ geometric_bayesian/operators/sym_operator.py
@@ -4,7 +4,7 @@
 import jax
 import jax.numpy as jnp
 from geometric_bayesian.operators.linear_operator import LinearOperator
-from geometric_bayesian.utils.types import Matrix, Scalar, Tuple, Optional, Key, Vector, Int
+from geometric_bayesian.utils.types import Matrix, Scalar, Tuple, Optional, Key, Vector, Int, Float
 
 from geometric_bayesian.linalg.eigvec_tridiagonal import eigvec_tridiagonal
 from geometric_bayesian.linalg.lanczos import lanczos
@@ -28,39 +28,31 @@ class SymOperator(LinearOperator):
     def diagonalize(
         self,
         rng_key: Optional[Key] = None,
+        method: Optional[str] = 'symeig',
         num_modes: Optional[Int] = None,
-        num_iterations: Optional[Int] = None,
-        method: Optional[str] = None
+        num_iterations: Optional[Int] = 100,
+        tol: Optional[Float] = None
     ) -> Tuple[Vector, Matrix]:
         r"""
         Return eigenvalues of the linear operator
         Eigenvectors calculation not available (https://github.com/jax-ml/jax/issues/14019)
         """
-        if rng_key is None:
-            rng_key = jax.random.key(0)
-        if num_modes is None:
-            num_modes = (0.3 * self.shape[0]).astype(int)
-        # assert num_modes <= self.size()[0], f"Number of modes requested exceeds opeartor dimension [{self.size()[0]}]"
-        if num_iterations is None:
-            num_iterations = 10
-        if method is None:
-            # method = 'lanczos' if dim >= 1e3 else 'symeig'
-            method = 'symeig'
-
         if method == 'symeig':
             d, v = jnp.linalg.eigh(self.dense()._mat)
         elif method == 'lanczos':
-            alpha, beta, v = lanczos(self, self.shape[0], num_modes, rng_key)
+            assert num_modes is not None
+            key, subkey = jax.random.split(rng_key if rng_key is not None else jax.random.key(0))
+            alpha, beta, v = lanczos(self, self.shape[0], num_modes, key)
             d = jax.scipy.linalg.eigh_tridiagonal(alpha, beta, eigvals_only=True)
-            v = jnp.matmul(v.T, eigvec_tridiagonal(rng_key, alpha, beta, d))
+            v = jnp.matmul(v.T, eigvec_tridiagonal(subkey, alpha, beta, d))
         elif method == 'lobpcg':
-            assert num_iterations is not None
+            assert num_modes is not None and num_iterations is not None
             mv_batch = jax.vmap(self, in_axes=(1,), out_axes=1)
-            d, v, _ = lobpcg_standard(mv_batch, jax.random.uniform(rng_key, shape=(self.shape[0], num_modes)), m=num_iterations)
+            d, v, _ = lobpcg_standard(mv_batch, jax.random.uniform(rng_key if rng_key is not None else jax.random.key(0),
+                                      shape=(self.shape[0], num_modes)), m=num_iterations, tol=tol)
         else:
             msg = "provide valid method ['lanczos', 'lobpcg']"
             ValueError(msg)
-
         return d, v
 
     # def diagonalize(
