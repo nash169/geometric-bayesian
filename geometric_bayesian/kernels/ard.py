3464b30ae1bccd65aa9b4d3faa885ff0eafdb335
Bernardo <bernardo.fichera@gmail.com>
2026-03-02 14:57:57 +0100

    Added scaling parameter.

--- geometric_bayesian/kernels/ard.py
+++ geometric_bayesian/kernels/ard.py
@@ -2,12 +2,13 @@
 # encoding: utf-8
 
 import jax.numpy as jnp
-from geometric_bayesian.utils.types import Vector
+from geometric_bayesian.utils.types import Vector, Scalar
 
 
 def ard(
     x: Vector,
     y: Vector,
-    l: Vector
+    l: Vector,
+    tau: Scalar = 1.0
 ):
     return jnp.exp(-.5 * jnp.sum(jnp.pow(x - y, 2) / l))
