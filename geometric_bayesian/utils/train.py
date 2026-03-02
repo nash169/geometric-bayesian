027f5fe12edc614fd7ff9a3758e1f654b5046476
Bernardo <bernardo.fichera@gmail.com>
2026-03-02 15:11:07 +0100

    Moved dataloader and added train with fixed batch and batch replacement.

--- geometric_bayesian/utils/train.py
+++ geometric_bayesian/utils/train.py
@@ -1,53 +1,190 @@
 import tqdm
 import optax
+import jax.random as jr
 import jax.numpy as jnp
 from flax import nnx
+from dataclasses import dataclass
+from typing import Callable
+
+
+@dataclass(frozen=True)
+class OptCfg:
+    lr: float = 1e-3
+    grad_clip: float = 1.0
+    weight_decay: float = 1e-4
+
+
+@dataclass(frozen=True)
+class TrainCfg:
+    opt: optax.GradientTransformation
+    steps: int = 20_000
+    batch_size: int = 64
+    seed: int = 0
+    batch_mode: str = "epoch"
+    verbose: bool = False
+    log: str = 'info'
+    record: bool = False
+
+
+class DataLoader:
+    def __init__(
+        self,
+        X: jnp.ndarray,
+        y: jnp.ndarray,
+        batch_size: int,
+        *,
+        shuffle: bool = True,
+        seed: int = 0,
+    ) -> None:
+        self.X = jnp.asarray(X)
+        self.y = jnp.asarray(y)
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+        self.key = jr.key(seed)
+        self.N = self.X.shape[0]
+        self.indices = jnp.arange(self.N)
+
+    def __iter__(self):
+        if self.shuffle:
+            self.key, subkey = jr.split(self.key)
+            self.indices = jr.permutation(subkey, self.N)
+        self.current_idx = 0
+        return self
+
+    def __next__(self):
+        if self.current_idx >= self.N:
+            raise StopIteration
+
+        start_idx = self.current_idx
+        end_idx = start_idx + self.batch_size
+        batch_indices = self.indices[start_idx:end_idx]
+        self.current_idx = end_idx
+
+        return self.X[batch_indices], self.y[batch_indices]
 
 
 def train(
     model,
-    dataloader,
-    loss_fn,
-    params: dict,
-    opt=optax.adam,
-    key=None,
-    verbose: bool = False,
-    record: bool = False
+    X: jnp.ndarray,
+    y: jnp.ndarray,
+    loss_fn: Callable,
+    cfg: TrainCfg,
 ):
-    assert "lr" in params, "define lerning rate [lr]"
-    assert "n_iters" in params, "define number of iterations [n_iters]"
+    opt = nnx.Optimizer(model, cfg.opt, wrt=nnx.Param)
+    dataloader = DataLoader(X, y, cfg.batch_size, shuffle=True, seed=cfg.seed)
 
-    optimizer = nnx.Optimizer(model, opt(params["lr"]), wrt=nnx.Param)
-    iters = tqdm.tqdm(range(params["n_iters"]), desc="Epoch", disable=not verbose)
+    @nnx.jit
+    def step(model, opt, x, y):
+        loss, grads = nnx.value_and_grad(loss_fn)(model, x, y)
+        opt.update(model, grads)
+        return loss
 
-    if key is None:
-        @nnx.jit
-        def step(model, optimizer, x, y):
-            loss, grads = nnx.value_and_grad(loss_fn, argnums=nnx.DiffState(0, nnx.Param))(
-                model, x, y
-            )
-            optimizer.update(model, grads)
-            return loss
-    else:
-        @nnx.jit
-        def step(model, optimizer, x, y, key):
-            (loss, key), grads = nnx.value_and_grad(loss_fn, argnums=nnx.DiffState(0, nnx.Param), has_aux=True)(
-                model, x, y, key
-            )
-            optimizer.update(model, grads)
-            return loss, key
+    if cfg.record:
+        log = []
 
-    loss_log, weights_log = [], []
-    for i in iters:
+    iters = tqdm.tqdm(range(cfg.steps), desc="Training w/o replacement", disable=not cfg.verbose)
+    for _ in iters:
         for x_tr, y_tr in dataloader:
-            if key is None:
-                loss = step(model, optimizer, x_tr, y_tr)
-            else:
-                loss, key = step(model, optimizer, x_tr, y_tr, key)
-            if verbose:
-                iters.set_postfix({"Loss": loss.item()})
-            if record:
-                loss_log.append(loss)
-                weights_log.append(model.params)
+            loss = step(model, opt, x_tr, y_tr)
+        if cfg.log == 'debug':
+            iters.set_postfix({"Loss": loss.item()})
+        if cfg.record:
+            log.append(model.params)
 
-    return jnp.array(loss_log), jnp.array(weights_log)
+    return jnp.array(log) if cfg.record else loss
+
+
+def train_batch_fixed(
+    model,
+    X: jnp.ndarray,
+    y: jnp.ndarray,
+    loss_fn: Callable,
+    cfg: TrainCfg,
+):
+    opt = nnx.Optimizer(model, cfg.opt, wrt=nnx.Param)
+    dataloader = DataLoader(X, y, cfg.batch_size, shuffle=True, seed=cfg.seed)
+
+    @nnx.jit
+    def step(model, opt):
+        for x_tr, y_tr in dataloader:
+            loss, grads = nnx.value_and_grad(loss_fn)(model, x_tr, y_tr)
+            opt.update(model, grads)
+        return loss
+
+    if cfg.record:
+        log = []
+
+    iters = tqdm.tqdm(range(cfg.steps), desc="Training w/o replacement [Fixed batch]", disable=not cfg.verbose)
+    for _ in iters:
+        loss = step(model, opt)
+        if cfg.log == 'debug':
+            iters.set_postfix({"Loss": loss.item()})
+        if cfg.record:
+            log.append(loss)
+
+    return jnp.array(log) if cfg.record else loss
+
+
+def train_batch_replacement(
+    model,
+    X: jnp.ndarray,
+    y: jnp.ndarray,
+    loss_fn: Callable,
+    cfg: TrainCfg,
+):
+    opt = nnx.Optimizer(model, cfg.opt, wrt=nnx.Param)
+
+    @nnx.jit
+    def step(model, opt, key):
+        idx = jr.randint(key, (cfg.batch_size,), 0, X.shape[0])
+        loss, grads = nnx.value_and_grad(loss_fn)(model, X[idx], y[idx])
+        opt.update(model, grads)
+        return loss
+
+    if cfg.record:
+        log = []
+
+    keys = jr.split(jr.key(cfg.seed), cfg.steps)
+    iters = tqdm.tqdm(range(cfg.steps), desc="Training with replacement]", disable=not cfg.verbose)
+    for t in iters:
+        loss = step(model, opt, keys[t])
+        if cfg.log == 'debug':
+            iters.set_postfix({"Loss": loss.item()})
+        if cfg.record:
+            log.append(loss)
+
+    return loss
+
+
+def train_stochastic_loss(
+    model,
+    X: jnp.ndarray,
+    y: jnp.ndarray,
+    loss_fn: Callable,
+    cfg: TrainCfg,
+):
+
+    opt = nnx.Optimizer(model, cfg.opt, wrt=nnx.Param)
+    dataloader = DataLoader(X, y, cfg.batch_size, shuffle=True, seed=cfg.seed)
+
+    @nnx.jit
+    def step(model, opt, x, y, key):
+        (loss, key), grads = nnx.value_and_grad(loss_fn, has_aux=True)(model, x, y, key)
+        opt.update(model, grads)
+        return loss, key
+
+    if cfg.record:
+        log = []
+
+    key = jr.key(cfg.seed)
+    iters = tqdm.tqdm(range(cfg.steps), desc="Training w/o replacement", disable=not cfg.verbose)
+    for _ in iters:
+        for x_tr, y_tr in dataloader:
+            loss, key = step(model, opt, x_tr, y_tr, key)
+        if cfg.log == 'debug':
+            iters.set_postfix({"Loss": loss.item()})
+        if cfg.record:
+            log.append(loss)
+
+    return jnp.array(log) if cfg.record else loss
