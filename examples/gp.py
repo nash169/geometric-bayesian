f2f8b8e0aaa4fa77663a755638c2f659b88d94bb
Bernardo <bernardo.fichera@gmail.com>
2026-03-23 11:11:07 +0100

    Added simple GP model with example.

--- examples/gp.py
+++ examples/gp.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 # encoding: utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
 
+# %%
 import os
 import sys
-
+import jax
+import jax.numpy as jnp
 import jax.random as jr
 import optax
+import matplotlib.pyplot as plt
 
 gb_root = os.path.abspath(
     os.path.join(os.getcwd(), "/Users/bernardo/Repos/geometric-bayesian")
@@ -13,26 +31,54 @@ gb_root = os.path.abspath(
 if not os.path.isdir(gb_root):
     gb_root = "/home/ubuntu/geometric-bayesian"
 sys.path.insert(0, gb_root)
-
 from geometric_bayesian.models.gp import GP
 from geometric_bayesian.kernels import rbf
 from geometric_bayesian.utils.train import TrainCfg, train
+from geometric_bayesian.utils.plot import plot, scatter
+from geometric_bayesian.utils.helper import make_sinusoid
+from geometric_bayesian.operators import PSDOperator
+
+# %%
+sinus_factor=5.0
+X_train, y_train, _, _, X_test, y_test = make_sinusoid(
+    n_train=150, n_valid=50, n_test=100,
+    interval=(-2.0,2.0),
+    noise=0.15,
+    sinus_factor=sinus_factor
+)
+
+# %%
+model = GP(dim=1, kernel=rbf)
+model.params = jnp.log(jnp.array([0.2,0.2,1.0e-2]))
+print(model.params)
+
+# %%
+fig = scatter(jnp.vstack((X_train, y_train)).T, color="blue", alpha=0.6, label="Training data")
+fig = scatter(jnp.vstack((X_test, y_test)).T, fig=fig, color="green", alpha=0.6, label="Test data")
+fig = plot(lambda x: jnp.sin(sinus_factor*x), fig=fig, range=[-5,5], color="black", linestyle="--", label="True function")
+fig = plot(
+    jax.vmap(model.posterior_mu(X_train,y_train)), 
+    fn_between=jax.vmap(lambda x: 2*jnp.sqrt(model.posterior_cov(X_train)(x))), 
+    fig=fig, range=[-5,5], color="red", label="Prediction"
+)
 
 
-def main() -> None:
-    model = GP(dim=3, kernel=rbf)
-    x = jr.uniform(jr.key(0), (10, 3))
-    y = jr.uniform(jr.key(0), (10,))
+# %%
+def loss_fn(m, x, y):
+    return -m(x, y) / x.shape[0]
 
-    def loss_fn(m, x, y):
-        return -m(x, y)
+cfg = TrainCfg(opt=optax.adam(1e-2), steps=1000, batch_size=X_train.shape[0])
+loss_val = train(model, X_train, y_train, loss_fn, cfg)
+print(model.params)
 
-    print(loss_fn(model, x, y))
-    cfg = TrainCfg(opt=optax.adam(1e-2), steps=10, batch_size=5)
-    loss_val = train(model, x, y, loss_fn, cfg)
+# %%
+fig = scatter(jnp.vstack((X_train, y_train)).T, color="blue", alpha=0.6, label="Training data")
+fig = scatter(jnp.vstack((X_test, y_test)).T, fig=fig, color="green", alpha=0.6, label="Test data")
+fig = plot(lambda x: jnp.sin(sinus_factor*x), fig=fig, range=[-5,5], color="black", linestyle="--", label="True function")
+fig = plot(
+    jax.vmap(model.posterior_mu(X_train,y_train)), 
+    fn_between=jax.vmap(lambda x: 2*jnp.sqrt(model.posterior_cov(X_train)(x))), 
+    fig=fig, range=[-5,5], color="red", label="Prediction"
+)
 
-    print("loss", float(loss_val))
-
-
-if __name__ == "__main__":
-    main()
+# %%
--- geometric_bayesian/models/gp.py
+++ geometric_bayesian/models/gp.py
@@ -9,22 +9,18 @@ import inspect
 
 from geometric_bayesian.utils.types import (
     Matrix,
-    Size,
-    List,
     Optional,
     Callable,
-    Array,
-    Int,
     Vector,
 )
+from geometric_bayesian.operators import PSDOperator
+from geometric_bayesian.utils.math import gram
+from geometric_bayesian.densities import MultivariateNormal
 from geometric_bayesian.utils.helper import (
     pytree_to_array,
     array_to_pytree,
     wrap_pytree_function,
 )
-from geometric_bayesian.operators import PSDOperator
-from geometric_bayesian.utils.math import gram
-from geometric_bayesian.densities import MultivariateNormal
 
 
 class GP(nnx.Module):
@@ -42,12 +38,14 @@ class GP(nnx.Module):
         key = jr.key(seed)
         self.noise = nnx.Param(jr.normal(key, shape=(1,)))
 
-        if ~isinstance(kernel, nnx.Module):
+        if not isinstance(kernel, nnx.Module):
             k_params = list(inspect.signature(kernel).parameters.values())[2:]
             keys = jax.random.split(key, len(k_params) + 1)
             self.kernel_params = nnx.List(
                 [
-                    nnx.Param(jr.normal(keys[i], shape=(dim if p.annotation.dims else 1,)))
+                    nnx.Param(
+                        jr.normal(keys[i], shape=(dim if p.annotation.dims else 1,))
+                    )
                     for i, p in enumerate(k_params)
                 ]
             )
@@ -55,7 +53,7 @@ class GP(nnx.Module):
 
         if mean is not None:
             key, subkey = jr.split(key)
-            if ~isinstance(mean, nnx.Module):
+            if not isinstance(mean, nnx.Module):
                 mu_params = list(inspect.signature(mean).parameters.values())[1:]
                 keys = jax.random.split(subkey, len(mu_params))
                 self.mean_params = nnx.List(
@@ -68,33 +66,52 @@ class GP(nnx.Module):
                 )
             self.mean = mean
 
-    def mu_fn(
-        self,
-        x: Vector
-    ):
-        return self.mean(x, *self.mean_params) if hasattr(self, "mean_params") else self.mean(x)
+    def mu_fn(self, x: Vector):
+        return (
+            self.mean(x, *self.mean_params)
+            if hasattr(self, "mean_params")
+            else self.mean(x)
+        )
 
-    def k_fn(
-        self,
-        x: Vector,
-        y: Vector
-    ):
-        return self.kernel(x, y, *self.kernel_params) if hasattr(self, "kernel_params") else self.kernel(x, y)
+    def k_fn(self, x: Vector, y: Vector):
+        return (
+            self.kernel(x, y, *self.kernel_params)
+            if hasattr(self, "kernel_params")
+            else self.kernel(x, y)
+        )
 
-    def cov_fn(
-        self,
-        x: Vector | Matrix
-    ):
-        return gram(self.k_fn, x, self.noise)
+    def cov_fn(self, x: Vector | Matrix):
+        noise = jnp.exp(self.noise).squeeze() + 1e-6
+        return gram(self.k_fn, x, noise)
 
-    def prior(
-        self,
-        x: Vector | Matrix
-    ):
+    def prior(self, x: Vector | Matrix):
         return MultivariateNormal(
             cov=PSDOperator(op=self.cov_fn(x), op_size=x.shape[0]),
             mean=self.mu_fn(x) if hasattr(self, "mean") else None,
         )
 
+    def posterior_mu(self, X, y):
+        k_xy = lambda x: jax.vmap(self.k_fn, in_axes=(None, 0))(x, X).squeeze()
+        prior_cov = PSDOperator(op=self.cov_fn(X), op_size=X.shape[0])
+        if hasattr(self, "mean"):
+            mean_X = self.mu_fn(X).squeeze()
+            return lambda x: self.mu_fn(x) + k_xy(x) @ prior_cov.solve(y - mean_X)
+        return lambda x: k_xy(x) @ prior_cov.solve(y)
+
+    def posterior_cov(self, X):
+        k_xy = lambda x: jax.vmap(self.k_fn, in_axes=(None, 0))(x, X).squeeze()
+        k_yx = lambda x: jax.vmap(self.k_fn, in_axes=(0, None))(X, x).squeeze()
+        prior_cov = PSDOperator(op=self.cov_fn(X), op_size=X.shape[0])
+        return lambda x: self.k_fn(x, x) - k_xy(x) @ prior_cov.solve(k_yx(x))
+
     def __call__(self, x: Vector, y: Vector):
         return self.prior(x)(y)
+
+    @property
+    def params(self) -> Vector:
+        return pytree_to_array(nnx.state(self))
+
+    @params.setter
+    def params(self, value):
+        nnx.update(self, array_to_pytree(value, nnx.state(self)))
+        # self = nnx.merge(nnx.split(self)[0], value)
