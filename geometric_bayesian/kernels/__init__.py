8af6c611a2c2442da2e8dccd68c2c85d4d544075
Bernardo <bernardo.fichera@gmail.com>
2026-03-23 11:13:26 +0100

    Minors.

--- geometric_bayesian/kernels/__init__.py
+++ geometric_bayesian/kernels/__init__.py
@@ -3,6 +3,11 @@
 
 from geometric_bayesian.kernels.rbf import rbf
 from geometric_bayesian.kernels.ard import ard
+from geometric_bayesian.kernels.periodic import periodic
 
 
-_all_ = []
+_all_ = [
+    "rbf",
+    "ard",
+    "periodic"
+]
