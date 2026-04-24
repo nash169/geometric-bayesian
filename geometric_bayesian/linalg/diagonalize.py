0f0e3ba10a7b2587b356fa2d9d9810055b23ea38
Bernardo <bernardo.fichera@gmail.com>
2026-04-24 09:52:05 +0200

    Added max number of modes cut for symeig.

--- geometric_bayesian/linalg/diagonalize.py
+++ geometric_bayesian/linalg/diagonalize.py
@@ -12,31 +12,27 @@ from jax.experimental.sparse.linalg import lobpcg_standard
 def diagonalize(
     mv: Callable,
     dim: Int,
+    rng_key: Optional[Key] = None,
     method: str = 'symeig',
     num_modes: Optional[Int] = None,
     num_iters: Int = 100,
     tol: Float = 1e-4,
-    rng_key: Optional[Key] = None,
 ) -> Tuple[Vector, Matrix]:
-    r"""
-    Return eigenvalues of the linear operator
-    Eigenvectors calculation not available (https://github.com/jax-ml/jax/issues/14019)
-    """
     if method == 'symeig':
-        mv_batch = jax.vmap(mv, in_axes=(1,), out_axes=1)
-        d, v = jnp.linalg.eigh(mv_batch(jnp.eye(dim)))
+        d, v = jnp.linalg.eigh(jax.vmap(mv, in_axes=(1,), out_axes=1)(jnp.eye(dim)))
         d, v = jnp.flip(d), jnp.flip(v, axis=1)
+        if num_modes is not None:
+            d, v = d[:num_modes], v[:, :num_modes]
     elif method == 'lanczos':
         assert num_modes is not None
         key, subkey = jax.random.split(rng_key if rng_key is not None else jax.random.key(0))
         alpha, beta, v = lanczos(mv, dim, num_modes, key)
         d = jax.scipy.linalg.eigh_tridiagonal(alpha, beta, eigvals_only=True)
-        v = jnp.matmul(v.T, eigvec_tridiagonal(subkey, alpha, beta, d))
+        v = jnp.matmul(v.T, eigvec_tridiagonal(subkey, alpha, beta, d))  # https://github.com/jax-ml/jax/issues/14019
     elif method == 'lobpcg':
         assert num_modes is not None
-        mv_batch = jax.vmap(mv, in_axes=(1,), out_axes=1)
-        d, v, _ = lobpcg_standard(mv_batch, jax.random.uniform(rng_key if rng_key is not None else jax.random.key(0),
-                                  shape=(dim, num_modes)), m=num_iters, tol=tol)
+        d, v, _ = lobpcg_standard(jax.vmap(mv, in_axes=(1,), out_axes=1), jax.random.uniform(
+            rng_key if rng_key is not None else jax.random.key(0), shape=(dim, num_modes)), m=num_iters, tol=tol)
     else:
         msg = "provide valid method ['symeig', 'lanczos', 'lobpcg']"
         ValueError(msg)
