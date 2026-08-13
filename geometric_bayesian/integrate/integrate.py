5b7216dfca47ad7549bc63c31dee97a41b8413a6
Bernardo <bernardo.fichera@gmail.com>
2026-08-13 15:24:21 +0200

    Simplified integrator and added first step to scan return.

--- geometric_bayesian/integrate/integrate.py
+++ geometric_bayesian/integrate/integrate.py
@@ -3,59 +3,32 @@
 
 import jax
 import jax.numpy as jnp
-from geometric_bayesian.utils.types import Callable, Optional, Array, Float, Key
-from geometric_bayesian.integrate.ef import ef
+import jax.random as jr
+
+from geometric_bayesian.utils.types import Callable, Optional
 
 
 def integrate(
     f: Callable,
-    u: Optional[Callable | Array] = None,
-    dt: Optional[Float] = 0.01,
-    T: Optional[Float] = 1.0,
-    integrator: Optional[Callable] = ef,
-    key: Optional[Key] = None,
+    integrator: Callable,
+    dt: float = 0.01,
+    T: float = 1.0,
+    u: Optional[Callable] = None,
+    seed: Optional[int] = None,
 ):
-    assert integrator is not None and dt is not None and T is not None
+    key = jr.key(seed) if seed is not None else None
 
     def step(carry, i):
         t = i * dt
         x, key = carry
+
+        # split key
         if key is not None:
-            key, subkey = jax.random.split(key)
-        if u is not None:
-            u_c = u(t, x) if isinstance(u, Callable) else u[i]
-        else:
-            u_c = None
-        x = integrator(f=f if key is None else lambda t, x, u: f(t=t, x=x, u=u_c, key=subkey), t=t, x=carry[0], u=u_c, dt=dt)
+            key, subkey = jr.split(key)
+
+        # integrate
+        x = integrator(f=f if key is None else lambda t, x, u: f(t=t, x=x, u=u, key=subkey), t=t, x=x, u=u, dt=dt)
+
         return (x, key), (x,)
 
-    def run(x):
-        history = jax.lax.scan(step, (x, key), jnp.arange(int(round(T / dt))))[1][0]
-        return (jnp.concatenate((jnp.expand_dims(x, axis=0), history), axis=0),)
-
-    return run
-
-    # def run(x):
-    #     history = jax.lax.scan(step, (x, key), jnp.arange(int(T / dt)))[1][0]
-    #     return (jnp.concatenate((jnp.expand_dims(x, axis=0), history), axis=0),)
-    #
-    # return run
-
-# def euler_forward(f, x, v, dt):
-#     vn = v + dt*f(x, v)
-#     xn = x + dt*v
-#     return xn, vn
-#
-# def integrate(
-#     f: Callable,
-#     dt: Optional[float] = 0.01,
-#     T: Optional[float] = 1.0,
-#     integrator: Optional[Callable] = euler_forward,
-# ):
-#     assert integrator is not None and dt is not None and T is not None
-#
-#     def step(carry, _):
-#         x, v = integrator(f=f, x=carry[0], v=carry[1], dt=0.01)
-#         return (x, v), (x, v)
-#
-#     return lambda x, v: jax.lax.scan(step, (x, v), None, length=int(T / dt))[1]
+    return lambda x: jnp.concatenate((jnp.expand_dims(x, axis=0), jax.lax.scan(step, (x, key), jnp.arange(int(round(T / dt))))[1][0]), axis=0)
